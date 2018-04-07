656173a268a12a876863abc46f5cb98574984d98
Pawan Kumar Singh <pspsingh972@gmail.com>
2018-04-07 13:10:23 +0530

    Completed Basic UI Mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { SearchResultComponent } from './search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/search-result.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-searchresult',
+    templateUrl: './search-result.component.html',
+    styleUrls: ['./search-result.component.scss']
+})
+export class SearchResultComponent {
+
+}
